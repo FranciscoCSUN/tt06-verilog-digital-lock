e77604f88c25652acb9f1ff1e039793cb93c2450
FranciscoCSUN <143661038+FranciscoCSUN@users.noreply.github.com>
2024-03-14 16:39:34 -0700

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -5,27 +5,42 @@
 */
 module tb ();
 
-  // Dump the signals to a VCD file. You can view it with gtkwave.
-  initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
-    #1;
-  end
+   // Dump the signals to a VCD file. You can view it with gtkwave.
+   initial begin
+      $dumpfile("tb.vcd");
+      $dumpvars(0, tb);
+      #1;
+   end
+   
+   // Wire up the inputs and outputs:
+   reg clk;
+   reg rst_n;
+   reg ena;
+   reg [7:0] ui_in;
+   reg [7:0] uio_in;
+   wire [7:0] uo_out;
+   wire [7:0] uio_out;
+   wire [7:0] uio_oe;
+   
+   input [3:0] button,
+   input clk,
+   input rst_n,
+   input ena,
+   output [6:0] ssd,
+   output [3:0] led,
+   output [3:0] dig);
+   
+   // Replace tt_um_example with your module name
+   tt_um_top user_project (
+      .button(button),
+      .clk(clk),
+      .rst_n(rst_n),
+      .ssd(ssd),
+      .led(led),
+      .dig(dig));
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-
-  // Replace tt_um_example with your module name:
-  tt_um_top user_project (
-
-      // Include power ports for the Gate Level test:
+   // why
+ /**     // Include power ports for the Gate Level test:
 `ifdef GL_TEST
       .VPWR(1'b1),
       .VGND(1'b0),
@@ -38,7 +53,7 @@ module tb ();
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
       .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+      .rst_n  (rst_n)     // not reset**/
   );
 
 endmodule
