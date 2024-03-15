3493bac0ad3b6ec37e3f5dd3e69501765e50adaf
Francisco Hernandez <francisco.hernandez.389@my.csun.edu>
2024-03-14 18:01:09 -0700

    changes

--- test/tb.v
+++ test/tb.v
@@ -15,7 +15,7 @@ module tb ();
   // Wire up the inputs and outputs:
   reg clk;
   reg rst_n;
-  reg ena;
+  //reg ena = 1;
   reg [7:0] ui_in;
   reg [7:0] uio_in;
   wire [7:0] uo_out;
@@ -36,9 +36,9 @@ module tb ();
       .uio_in (uio_in),   // IOs: Input path
       .uio_out(uio_out),  // IOs: Output path
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
+      //.ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+      .rstn  (rst_n)     // not reset
   );
 
 endmodule
